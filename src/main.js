640baf1762974b9fa0987eaa9f89af5526bde3c7
muzi <xiumuds@163.com>
2021-12-05 19:28:18 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import { Button, Select, Container, Aside, Main, Header, Menu, MenuItem, Submenu, MenuItemGroup } from 'element-ui';
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+// Vue.component(Button.name, Button);
+// Vue.component(Select.name, Select);
+// Vue.component(Container.name, Container);
+// Vue.component(Aside.name, Aside);
+// Vue.component(Main.name, Main);
+// Vue.component(Header.name, Header);
+// Vue.use(Menu)
+// Vue.use(MenuItem)
+// Vue.use(Submenu)
+// Vue.use(MenuItemGroup)
 
 new Vue({
   router,
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,152 @@
+<template>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px;">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg">
+          <div class="userInfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登陆时间：<span>2020-12-4</span></p>
+          <p>上次登陆地点：<span>南京</span></p>
+        </div>
+      </el-card>
+      <el-card style="margin-top: 20px; height: 480px;">
+        <el-table :data="tableData">
+          <el-table-column
+            show-overflow-tooltip
+            v-for="(val, key) in tableLabel"
+            :key="key"
+            :prop="key"
+            :label="val"
+          >
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+    <el-col :span="16" style="margin-top: 20px;">
+      <div class="num">
+        <el-card
+          shadow="hover"
+          v-for="item in countData"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i
+            class="icon"
+            :class="`el-icon-${item.icon}`"
+            :style="{ background: item.color }"
+          ></i>
+          <div class="detail">
+            <p class="num">¥{{ item.value }}</p>
+            <p class="txt">¥{{ item.name }}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card shadow="hover" style="height: 280px"></el-card>
+      <div class="graph">
+        <el-card shadow="hover" style="height: 280px"></el-card>
+        <el-card shadow="hover" style="height: 280px"></el-card>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userImg: require('../../assets/images/user.jpg'),
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100000000000000,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'iPhone',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'HUAWEI',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'Xiaomi',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'Meizu',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
+      tableLabel: {
+        name: '课程',
+        todayBuy: '今日购买',
+        monthBuy: '本月购买',
+        totalBuy: '总购买'
+      },
+      countData: [
+        {
+          name: '今日已支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '今日收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '今日未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        },
+        {
+          name: '本月支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '本月收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '本月未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/home'
+</style>
